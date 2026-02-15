c44f548a0bb25f0cb92bdb40c58583d1cb514997
4hm3d57 <4hm3d57@gmail.com>
2026-02-16 00:46:17 +0300

    fix: improve graceful shutdown logging and error handling

---
+++ README.md
@@ -0,0 +1,24 @@
+# Hire Service
+
+Backend service for Hire Platform build with:
+
+- Go
+- Gin
+- Zap Logger
+
+
+## Featuers
+
+- Health check endpoint
+- Config management
+- Structured logging
+
+## Run
+
+go run  main.go
+
+
+## Env
+
+HTTPport=4000
+AppEnv=development
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,7 +10,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
-	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"context"
+	"time"
 )
 
 
@@ -36,17 +41,45 @@ func main() {
 	// gin http framework
 	router := gin.New()
 	router.Use(gin.Recovery())
-
 	
 	router.GET("/health", func(c *gin.Context){
 		c.JSON(200, gin.H{"status": "ok"})
 	})
-
-	zlogs.Info("Server started", zap.String("port", cfg.HTTPport))
-
-
-	err = router.Run(":" + cfg.HTTPport)
-	if err != nil {
-		log.Fatal("Failed to start server", zap.Error(err))
+	
+	// creating http server config
+	server := &http.Server {
+		Addr: ":" + cfg.HTTPport,
+		Handler: router,
 	}
+	
+
+	// start server in goroutine
+	go func() {
+		zlogs.Info("Server started", zap.String("port", cfg.HTTPport))
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed{
+			zlogs.Fatal("Server failed", zap.Error(err))
+		}
+	}()
+
+	
+	// create channel signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	
+	zlogs.Info("Shutdown signal received")
+
+	// shutdown server gracefuly
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+
+	if err = server.Shutdown(ctx); err != nil {
+		zlogs.Fatal("Shutdown failed", zap.Error(err))
+	}
+
+	zlogs.Info("Closing database connection")
+
+	zlogs.Info("Server exited cleanly")
 }
